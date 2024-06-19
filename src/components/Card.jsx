70f2da584d37f78d537db27652b3023b811dbe34
gitnarayan <107856223+gitnarayan@users.noreply.github.com>
2024-06-19 17:17:55 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 
-const Card = ({ object }) => {
+const Card = ({ object, onRemove }) => {
   return (
     <div
-      className="card shadow  mb-3"
-      style={{ maxWidth: "18rem" }}
+      className="card shadow mb-3"
+      style={{ maxWidth: "24rem" }}
       key={object.id}
     >
       <div className="d-flex justify-content-end">
-        {/* <div className="card-header">Header</div> */}
-        <i className="fa-solid fa-xmark text-danger m-3 fs-4"></i>
+        <i className="fa-solid fa-xmark text-danger m-3 fs-4" onClick={onRemove}></i>
       </div>
       <div className="card-body">
         <h5 className="card-title">{object.title}</h5>
         <p className="card-text">{object.body}</p>
-
       </div>
       <div className="w-100% p-2">
-        <img  className = 'img-fluid' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSccMa6zJqKZR_Lo61ICnwt3DXwgPBhzq_sAA&s" alt="" srcset="" />
+        <img
+          className="img-fluid w-100"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSccMa6zJqKZR_Lo61ICnwt3DXwgPBhzq_sAA&s"
+          alt=""
+        />
       </div>
     </div>
   );
